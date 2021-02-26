2490c1cf6e42c46746f9c19a5f07b0e8648152d4
ksv23 <kringsv7@gmail.com>
2021-02-25 23:54:27 -0500

    Lesson continue

--- Lessons/Week1/README.md
+++ Lessons/Week1/README.md
@@ -4,3 +4,16 @@
 **NOTES on ARRAY**: 
 1. Arrays are ordered collection of values.
 2. Use square brackets []
+3. Arrays are indexed [0,1,2,3,4]
+Array Methods:
+4. .push - add to the end 
+5. .pop - remove from end
+6. .shift - remove from start
+7. .unshift - add to start
+More Methods:
+8. .concat - merge two or more arrays
+9. .includes - look for a value
+10. .indexOf - just like string.indexOf
+11. .reverse() - reverses an array
+12. .slice - copies a portion on an array
+13. .splice - removes/replaces elementsxw
--- Lessons/Week1/arrays.js
+++ Lessons/Week1/arrays.js
@@ -1,2 +1,21 @@
 let days = ['Mon', 'Tues', 'Wed'];
 console.log(days[0]); //to access index 0 of array: days[0]
+days[3] = 'thurs'; //to set index 3 of array equal to thurs
+
+////CONCAT METHOD
+let cats = ['blue', 'pink'];
+let dogs = ['red', 'orange'];
+let combo = dogs.concat(cats);
+//>>combo
+//>>(4) ["red", "orange", "blue", "pink"]
+
+////Slice
+let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+let coolColors = colors.slice(3);
+//>>coolColors
+//>>(4) ["green", "blue", "indigo", "violet"]
+let warmColors = colors.slice(0, 3);
+//>>warmColors
+//>>(3)["red", "orange", "yellow"]
+
+////Splice Method
